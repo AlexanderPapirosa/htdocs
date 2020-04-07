076d5d79d35232c310df655c59ca815697040b92
Alexander <info@papirosa.com>
2020-04-07 17:18:30 +0200

    Initial commit

---
+++ Temp/index.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+<link rel="stylesheet" href="css/index.css" type="text/css" />
+
+<title>
+TRS.PAPIROSA.COM - TIME RECORDING SYSTEM BY EDV-KEIL.DE
+</title>
+</head>
+<body>
+<table align="center" border="0" cellspacing="0" cellpadding="0">
+
+<tr>
+<td width="*"></td>
+<td width="500">
+<div id="head_index">
+<div id="head_index_text">
+TimeRecordingSystem 1.1
+</div>
+</div>
+</td>
+<td width="*"></td>
+</tr>
+
+<tr>
+<td width="*"></td>
+<td width="500">
+
+<div id="body_index">
+<div id="body_index_text">
+Wir verwenden Cookies. Durch Bestätigen des Buttons "Akzeptieren" stimmen Sie der Verwendung zu.
+</div>
+
+<table align="center" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td>
+<div id="body_index_link">
+Ablehnen
+</div>
+</td>
+<td>
+<div id="body_index_link">
+Akzeptieren
+</div>
+</td>
+</tr>
+</table>
+</div>
+
+</td>
+<td width="*"></td>
+</tr>
+
+<tr>
+<td width="*"></td>
+<td width="500">
+<div id="footer_index">
+<div id="footer_index_text">
+AGB - Impressum - Powered by EDV-Keil
+</div>
+</div>
+</td>
+<td width="*"></td>
+</tr>
+
+</table>
+</body>
+</html>
