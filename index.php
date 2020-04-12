b1bef3d1681904b55bfbee89fba74300efb54ba6
Alexander <info@papirosa.com>
2020-04-12 19:04:13 +0200

    remove unusable table

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<html>
-<!-- DB-NR: 1 -->
+<!DOCTYPE html>
+<html lang="de">
+<!-- site: 1 -->
 <head>
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="shortcut icon" href="graphic/icon.png" type="image/x-icon" />
-<link rel="stylesheet" type="text/css" /href="Css/mainFormat.css" >
+<link rel="stylesheet" type="text/css" href="css/main.css" >
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
 <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
 
@@ -15,34 +16,20 @@ TRS.PAPIROSA.COM - TIME RECORDING SYSTEM BY EDV-KEIL.DE
 </head>
 
 <body>
-
-<table align="center" border="0" cellspacing="0" cellpadding="0">
-<!-- HEAD -->
-<tr>
-	<td width="500">
-		<div id="head">
-		<div id="headText">
+	<div id="wrapper">
+	
+		<header>
 			TimeRecordingSystem 1.3
-		</div>
-		</div>
-	</td>
-</tr>
-<!-- MENU -->
-<tr align="center">
-	<td align="center"  width="500">
-		<div id="menu">
-			<ul align="center">
-				<!-- DB-NR: 1 -->
-				<li><a href="#">Hilfe</a></li>
+		</header>
+		
+		<nav id="menu">
+			<ul>
+				<!-- menu: 1 -->
+				<li><a href="test.php">Hilfe</a></li>
 			</ul>
-		</div>
-	</td>
-</tr>
-<!-- BODY -->
-<tr>
-	<td width="500">
-		<div id="body">
-			<br>
+		</nav>
+
+		<section>
 			<table border="0" cellpadding="0" cellspacing="40" align="center" style="width:100%">
 				<tr>
 					<td align="center">  
@@ -69,43 +56,36 @@ TRS.PAPIROSA.COM - TIME RECORDING SYSTEM BY EDV-KEIL.DE
 				<a href="#">Passwort vergessen?</a><br>
 				<a href="#">Registrieren</a><br>
 				<a href="#">Registrieren als Gruppenadministrator</a>
-			</div> 
-		</div>
-	</td>
-</tr>
-<!-- FOOTER -->
-<tr align="center">
-	<td width="500">
-		<div id="footer">
-			<div id="footerText">
-				<a href="http://www.edv-keil.de">Powered by EDV-Keil &copy; 2020</a> 
 			</div>
-		</div>
-	</td>
-</tr>
+			<br>
+		</section>
+		
+		<footer>
+			<a href="http://www.edv-keil.de">Powered by EDV-Keil &copy; 2020</a>
+		</footer>
 
-</table>
-
-<script>
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#89FF88",
-      "text": "#005500"
-    },
-    "button": {
-      "background": "#99CC99",
-      "text": "#005500"
-    }
-  },
-  "theme": "classic",
-  "content": {
-	"message": "Diese Webseite verwendet Cookies, um Ihnen ein angenehmeres Surfen zu ermöglichen.",
-    "dismiss": "Verstanden!"
-  }
-});
-</script>
-
+	</div>
+	
+	<script>
+		window.cookieconsent.initialise({
+		  "palette": {
+			"popup": {
+			  "background": "#89FF88",
+			  "text": "#005500"
+			},
+			"button": {
+			  "background": "#99CC99",
+			  "text": "#005500"
+			}
+		  },
+		  "theme": "classic",
+		  "content": {
+			"message": "Diese Webseite verwendet Cookies, um Ihnen ein angenehmeres Surfen zu ermöglichen.",
+			"dismiss": "Verstanden!"
+		  }
+		});
+	</script>
+	
 </body>
 
 </html>
